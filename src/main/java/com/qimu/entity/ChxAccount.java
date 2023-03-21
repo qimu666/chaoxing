cd1bad02468d945ce16e58fb8e2a5303d0f0b213
qimu <2483482026@qq.com>
2023-03-21 22:12:05 +0800

    超星学习通视频

---
+++ src/main/java/com/qimu/entity/ChxAccount.java
@@ -0,0 +1,28 @@
+package com.qimu.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class ChxAccount {
+    private String username;
+    private String password;
+
+    private String userid;
+
+    private String cookie;
+
+    private List<ChxCourse> allCourse;
+
+    private ChxCourse curCourse;
+
+    private Boolean isRunning;
+
+    public ChxAccount(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/qimu/entity/ChxResource.java
@@ -0,0 +1,14 @@
+package com.qimu.entity;
+
+
+import lombok.Data;
+
+@Data
+public class ChxResource {
+    private String videoName;
+    private String dToken;
+    private int videoDuration;
+    private String objectId;
+    private String jobId;
+    private String otherInfo;
+}
